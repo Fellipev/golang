af0ac171992966cf58df448eee934b4d8916362f
Fellipev <fellipe2reis@gmail.com>
2024-09-11 16:25:28 -0300

    first commit

---
+++ 1-Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+//Registra uma mensagem estatica
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
+}
---
+++ 1-Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+//Retorna uma mensagem estatica 2
+func escrever2() {
+	fmt.Println("Escrevendo pela auxiliar 2")
+}
