df01b0470497ccb4591f8cfe925cf90d0523b876
Fellipev <fellipe2reis@gmail.com>
2024-09-13 19:05:00 -0300

    tipo de dados

---
+++ 2- vars/tipos.go
@@ -0,0 +1,22 @@
+package main
+
+func tipos() {
+
+	//Inteiros
+
+	//int8, int16, int32(rune), int64
+
+	//Inteiros sem sinal
+
+	//uint8(byte), uint16, uint32, uint64
+
+	//float32, float64
+
+	//String = "", char (não tem o tipo char, mas usa-se '')
+
+	//bool
+
+	//error
+
+	//Valor zero
+}
