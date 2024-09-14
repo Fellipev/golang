2844a2ee45deaf6cfc8330ee0e921a56e0da17c5
Fellipev <fellipe2reis@gmail.com>
2024-09-13 23:23:45 -0300

    Herança e relacionamento de struct

--- 4 - structs/struct.go
+++ 4 - structs/struct.go
@@ -2,19 +2,36 @@ package main
 
 import "fmt"
 
-type usuario struct {
-	nome   string
-	idade  int8
-	altura float32
+type pessoa struct {
+	nome     string
+	idade    int8
+	altura   float32
+	endereco endereco //assim é uma relação de classe
+}
+
+type endereco struct {
+	logradouro string
+	numero     int
+}
+
+type estudante struct {
+	pessoa    //Herança feita
+	curso     string
+	faculdade string
 }
 
 func main() {
-	var u usuario
+	var u pessoa
 	u.nome = "Fellipe"
 	u.idade = 21
 	u.altura = 1.72
 
-	u2 := usuario{"Giovanna", 21, 1.68}
+	u2 := pessoa{"Giovanna", 21, 1.68, endereco{"Giovanni Gronchi", 30}}
+
+	e := estudante{u, "ADS", "SENAC"}
+	e2 := estudante{u2, "Odontologia", "UNESP"}
 
 	fmt.Println(u, u2)
+	fmt.Println("________")
+	fmt.Println(e, e2)
 }
